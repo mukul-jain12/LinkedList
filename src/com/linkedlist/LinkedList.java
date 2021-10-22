3ecf3a327e6575c30aac516d1d181ee4ea0a4f99
Mukul Jain <mukuljain422@gmail.com>
2021-10-22 22:38:58 +0530

    Simple linked list

--- src/com/linkedlist/LinkedList.java
+++
@@ -1,7 +0,0 @@
-package com.linkedlist;
-
-public class LinkedList {
-	public static void name() {
-		System.out.println("Welcome To Linked List Problem");
-	}
-}
---
+++ src/com/linkedlist/LinkedListProblem.java
@@ -0,0 +1,16 @@
+package com.linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedListProblem {
+	public static void main(String args[]) {
+		System.out.println("Welcome To Linked List Problem");
+		
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		
+		System.out.println(list);
+	}
+}
