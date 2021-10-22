a7672c3b0151d8e845ebc831b3a35138a8dc50af
Mukul Jain <mukuljain422@gmail.com>
2021-10-22 23:00:30 +0530

    simple linked list

--- src/com/linkedlist/LinkedListProblem.java
+++ src/com/linkedlist/LinkedListProblem.java
@@ -4,13 +4,28 @@ import java.util.LinkedList;
 
 public class LinkedListProblem {
 	public static void main(String args[]) {
-		System.out.println("Welcome To Linked List Problem");
-		
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		list.add(56);
-		list.add(30);
-		list.add(70);
-		
-		System.out.println(list);
+		Node firstNode = new Node(56);
+		Node secondNode = new Node(30);
+		Node thirdNode = new Node(70);
+
+
+		Node head = firstNode;
+		firstNode.next = secondNode;
+		secondNode.next = thirdNode;
+		Node tail = thirdNode;
+
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+}
+
+class Node{
+	int data;
+	Node next;
+	public Node(int info) {
+		this.data = info;
 	}
 }
